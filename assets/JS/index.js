fbc8b38e4b55912fa7ec3301b8843248e11adad2
Oleksandra <oleksandrashkutova@gmail.com>
2025-10-27 18:08:21 +0200

    Add userCard with react

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,5 +1,5 @@
 
-
+/*
 // el = document.createElement(type)
 // el.textContent = 'content'
 // el.title = ''
@@ -7,12 +7,12 @@
 // React.createReactElement(type, attrs, content)
 
 // JSX - спец. синтаксис (синт. цукор)
-/* <type atrrs>content</type> */
+// <type atrrs>content</type> 
 
 // Babel: jsx => js
 
 //1. доступ до улумента розмітки div з id='root'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 
 //2. вбудовуємо за допомогою метода .render компонент App
 root.render(<App />);
@@ -96,3 +96,65 @@ function NewsSection (){
     </article>
   );
 };
+*/
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+
+function App(){
+  const user = {
+    firstName: 'Vika',
+    lastName: 'Lyn',
+    location: {
+      country: 'Ukraine',
+      city: 'Kyiv',
+    },
+    photo: 'https://t4.ftcdn.net/jpg/03/83/25/83/360_F_383258331_D8imaEMl8Q3lf7EKU2Pi78Cn0R7KkW9o.jpg',
+    statistic: {
+      tweets: 1337,
+      following: 561,
+      followers: 718,
+    },
+    isMale: 'false',
+  };
+  
+  return(
+    <>
+      <UserCard userInfo = {user}/>
+    </>
+  ); 
+};
+
+function UserCard(props){
+  const {firstName, lastName, location, photo, statistic, isMale } = props.userInfo;
+  const styleCard = {
+    boxShadow: isMale === 'true' ? '0 0 10px blue' : '0 0 10px red',
+  };
+  return (
+    <article className = "userCard">
+      <img style={styleCard} src={photo} alt={firstName}/>
+      <div>
+        <h2>{firstName} {lastName}</h2>
+        <h3>{location.country}, {location.city}</h3>
+      </div>
+      <button>
+        <i className="fa-solid fa-heart"></i>
+      </button>
+      <div className='statistic'>
+        <span>
+          <p>Tweets</p>
+          <p>{statistic.tweets}</p>
+        </span>
+        <span>
+          <p>Folloving</p>
+          <p>{statistic.following}</p>
+        </span>
+        <span>
+          <p>Followers</p>
+          <p>{statistic.followers}</p>
+        </span>
+      </div>
+    </article>
+  )
+}
+
